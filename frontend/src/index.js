7427ea9ead298c389780cbfc8eb239869ef553d1
tush-tr <tusharrajput604@gmail.com>
2023-07-23 01:30:48 +0530

    ✨ feat(Auth0): Implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Auth0ProviderWithHistory from "./auth/Auth0Provider";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
   </React.StrictMode>
 );
 
